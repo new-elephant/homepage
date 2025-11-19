65a31c40a6fb18aa4aa53432651f9b9ce74d431e
othree <othree@gmail.com>
2025-11-19 16:46:28 +0900

    Add two new products

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,40 +1,40 @@
-import en from './en.json';
-import zhTw from './zh-tw.json';
+import en from "./en.json";
+import zhTw from "./zh-tw.json";
 
 export const languages = {
-  en: 'English',
-  'zh-tw': '繁體中文',
+  en: "English",
+  "zh-tw": "繁體中文",
 };
 
-export const defaultLang = 'zh-tw';
+export const defaultLang = "zh-tw";
 
 export const translations = {
   en: en,
-  'zh-tw': zhTw,
+  "zh-tw": zhTw,
 };
 
 export type Language = keyof typeof languages;
 
 export function getLangFromUrl(url: URL): Language {
-  const [, lang] = url.pathname.split('/');
+  const [, lang] = url.pathname.split("/");
   if (lang in languages) return lang as Language;
   return defaultLang;
 }
 
 export function useTranslations(lang: Language) {
   return function t(key: string) {
-    const keys = key.split('.');
+    const keys = key.split(".");
     let value: any = translations[lang];
 
     for (const k of keys) {
-      if (value && typeof value === 'object') {
+      if (value && typeof value === "object") {
         value = value[k];
       } else {
         return key;
       }
     }
 
-    return value || key;
+    return value !== undefined ? value : key;
   };
 }
 
